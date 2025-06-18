3cd007db18ba07ebb33203d087c1c4281158acf1
Augusto Brandão <gutoo.brandao@gmail.com>
2025-06-18 17:01:39 -0300

    User entity and resource

---
+++ src/main/java/br/com/jna/sistema_loja/resources/UserResource.java
@@ -0,0 +1,17 @@
+package br.com.jna.sistema_loja.resources;
+
+import br.com.jna.sistema_loja.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "123456", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
