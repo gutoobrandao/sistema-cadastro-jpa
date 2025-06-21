8cd182a340addff964972d12db9d460d3f0c51ea
Augusto Brandão <gutoo.brandao@gmail.com>
2025-06-21 09:00:07 -0300

    Subtotal & Total methods

--- src/main/java/br/com/jna/sistema_loja/entities/Payment.java
+++ src/main/java/br/com/jna/sistema_loja/entities/Payment.java
@@ -1,5 +1,6 @@
 package br.com.jna.sistema_loja.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -19,6 +20,7 @@ public class Payment implements Serializable {
 
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
