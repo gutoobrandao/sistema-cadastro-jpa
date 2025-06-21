58a7a3fb994a63d734ab32e9daed0eb7383dc70e
Augusto Brandão <gutoo.brandao@gmail.com>
2025-06-21 13:33:21 -0300

    Exception handling - delete

--- src/main/java/br/com/jna/sistema_loja/services/UserService.java
+++ src/main/java/br/com/jna/sistema_loja/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.jna.sistema_loja.services;
 
 import br.com.jna.sistema_loja.entities.User;
 import br.com.jna.sistema_loja.repositories.UserRepository;
+import br.com.jna.sistema_loja.services.exceptions.DatabaseException;
 import br.com.jna.sistema_loja.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
